ab202bf8ab21bdb02d8bbf873a6ef9741ac6c55e
Tewelde Raya <temesgentaddesse5@gmail.com>
2026-01-04 00:37:50 +0300

    commit for folder structure of repository

--- Frontend/user/user.js
+++
@@ -1,6 +0,0 @@
-function viewProducts() {
-  fetch("http://localhost:3000/api/products")
-    .then(res => res.json())
-    .then(data => products.innerHTML =
-      JSON.stringify(data, null, 2));
-}
